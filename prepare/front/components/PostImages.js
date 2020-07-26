df0ab1101c16910ad6fffe7f296a65db84397303
Tia Hwang <tiajshwang@gmail.com>
2020-07-26 21:35:42 +0900

    ImageZoom + ImageSlide

--- prepare/front/components/PostImages.js
+++ prepare/front/components/PostImages.js
@@ -1,8 +1,78 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import PropTypes from "prop-types";
+import styled from "styled-components";
+import { PlusOutlined } from "@ant-design/icons";
+
+import ImagesZoom from "./ImagesZoom";
+
+const ImageContainer = styled.div`
+    display: inline-block;
+    width: 50%;
+    text-align: center;
+    vertical-align: middle;
+`;
 
 const PostImages = ({ images }) => {
-    return <div>구현중...</div>;
+    const [showImagesZoom, setShowImagesZoom] = useState(false);
+    const onZoom = useCallback(() => {
+        setShowImagesZoom(true);
+    }, []);
+
+    const onClose = useCallback(() => {
+        setShowImagesZoom(false);
+    }, []);
+
+    if (images.length === 1) {
+        return (
+            <>
+                <img role="presentation" src={images[0].src} alt={images[0].src} onClick={onZoom} />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
+        );
+    }
+
+    if (images.length === 2) {
+        return (
+            <>
+                <img
+                    role="presentation"
+                    style={{ display: "inline-block", width: "50%" }}
+                    src={images[0].src}
+                    alt={images[0].src}
+                    onClick={onZoom}
+                />
+                <img
+                    role="presentation"
+                    style={{ display: "inline-block", width: "50%" }}
+                    src={images[1].src}
+                    alt={images[1].src}
+                    onClick={onZoom}
+                />
+                {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+            </>
+        );
+    }
+
+    return (
+        <>
+            <div>
+                <img
+                    role="presentation"
+                    style={{ width: "50%" }}
+                    src={images[0].src}
+                    alt={images[0].src}
+                    onClick={onZoom}
+                />
+                <ImageContainer role="presentation" onClick={onZoom}>
+                    <PlusOutlined />
+                    <br />
+                    {images.length - 1}
+                    개의 사진 더보기
+                </ImageContainer>
+            </div>
+            {showImagesZoom && <ImagesZoom images={images} onClose={onClose} />}
+        </>
+    );
 };
 
 PostImages.propTypes = {
