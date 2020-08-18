86fc7f14026a1f06c01ddee022250148a3923ed7
Tia Hwang <tiajshwang@gmail.com>
2020-08-18 18:32:19 +0900

    redux-saga: state, action

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -1,5 +1,13 @@
 import { all, fork, takeLatest, put, delay } from "redux-saga/effects";
 import axios from "axios";
+import {
+    ADD_POST_REQUEST,
+    ADD_POST_SUCCESS,
+    ADD_POST_FAILURE,
+    ADD_COMMENT_REQUEST,
+    ADD_COMMENT_SUCCESS,
+    ADD_COMMENT_FAILURE,
+} from "../reducers/post";
 
 function addPostAPI(data) {
     return axios.post("/api/post", data);
@@ -10,13 +18,33 @@ function* addPost(action) {
         // const result = yield call(addPostAPI, action.data);
         yield delay(1000);
         yield put({
-            type: "ADD_POST_SUCCESS",
+            type: ADD_POST_SUCCESS,
             // data: result.data,
         });
     } catch (err) {
         yield put({
-            type: "ADD_POST_FAILURE",
-            data: err.response.data,
+            type: ADD_POST_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
+
+function addCommentAPI(data) {
+    return axios.post(`/api/post/${data.postId}/comment`, data);
+}
+
+function* addComment(action) {
+    try {
+        // const result = yield call(addCommentAPI, action.data);
+        yield delay(1000);
+        yield put({
+            type: ADD_COMMENT_SUCCESS,
+            // data: result.data,
+        });
+    } catch (err) {
+        yield put({
+            type: ADD_COMMENT_FAILURE,
+            error: err.response.data,
         });
     }
 }
@@ -24,9 +52,13 @@ function* addPost(action) {
 // takeLatest: 2번 요청 => 2번 응답이 아니라 제일 마지막 응답만 1번 전송. 요청은 그대로 2번 감 => 서버 쪽에서 데이터 중복 체크하기
 // throttle: 서버에 요청 보내는 시간까지 정할 수 있음. 그 시간안에는 요청 1번만 전송되는 것
 function* watchAddPost() {
-    yield takeLatest("ADD_POST_REQUEST", addPost);
+    yield takeLatest(ADD_POST_REQUEST, addPost);
+}
+
+function* watchAddComment() {
+    yield takeLatest(ADD_COMMENT_REQUEST, addComment);
 }
 
 export default function* postSaga() {
-    yield all([fork(watchAddPost)]);
+    yield all([fork(watchAddPost), fork(watchAddComment)]);
 }
