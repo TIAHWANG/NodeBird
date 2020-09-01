b21ceea83de62de9b45c183a9b5837e4d38d8c3d
Tia Hwang <tiajshwang@gmail.com>
2020-09-01 18:41:47 +0900

    infinite scrolling

--- prepare/front/sagas/post.js
+++ prepare/front/sagas/post.js
@@ -1,4 +1,4 @@
-import { all, fork, takeLatest, put, delay } from "redux-saga/effects";
+import { all, fork, takeLatest, put, delay, throttle } from "redux-saga/effects";
 import axios from "axios";
 import shortId from "shortid";
 import {
@@ -11,8 +11,33 @@ import {
     REMOVE_POST_REQUEST,
     REMOVE_POST_SUCCESS,
     REMOVE_POST_FAILURE,
+    LOAD_POST_REQUEST,
+    LOAD_POST_SUCCESS,
+    LOAD_POST_FAILURE,
+    generateDummyPost,
 } from "../reducers/post";
 import { ADD_POST_TO_ME, REMOVE_POST_OF_ME } from "../reducers/user";
+
+function loadPostAPI(data) {
+    return axios.get("/api/post", data);
+}
+
+function* loadPost() {
+    try {
+        // const result = yield call(loadPostAPI, action.data);
+        yield delay(1000);
+        yield put({
+            type: LOAD_POST_SUCCESS,
+            data: generateDummyPost(10),
+            // data: result.data,
+        });
+    } catch (err) {
+        yield put({
+            type: LOAD_POST_FAILURE,
+            error: err.response.data,
+        });
+    }
+}
 
 function addPostAPI(data) {
     return axios.post("/api/post", data);
@@ -94,6 +119,10 @@ function* addComment(action) {
 // takeEvery: 비동기적으로 동작
 // takeLatest: 동시에 2번 요청 => 2번 응답이 아니라 제일 마지막 응답만 1번 전송. 요청은 그대로 2번 감 => 서버 쪽에서 데이터 중복 체크하기
 // throttle: 서버에 요청 보내는 시간까지 정할 수 있음. 그 시간안에는 요청 1번만 전송되는 것
+function* watchLoadPost() {
+    yield throttle(5000, LOAD_POST_REQUEST, loadPost);
+}
+
 function* watchAddPost() {
     yield takeLatest(ADD_POST_REQUEST, addPost);
 }
@@ -107,5 +136,10 @@ function* watchAddComment() {
 }
 
 export default function* postSaga() {
-    yield all([fork(watchAddPost), fork(watchRemovePost), fork(watchAddComment)]);
+    yield all([
+        fork(watchLoadPost),
+        fork(watchAddPost),
+        fork(watchRemovePost),
+        fork(watchAddComment),
+    ]);
 }
